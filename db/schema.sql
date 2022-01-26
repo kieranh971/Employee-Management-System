f9d10603c50c8e003f9fa19c65e5f2252e041fb5
Kieran Harris <kharris19063@gmail.com>
2022-01-25 19:06:57 -0500

    Updating schema and seeds files

--- db/schema.sql
+++ db/schema.sql
@@ -13,7 +13,7 @@ CREATE TABLE roles (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
     department_id INT NOT NULL,
-    FOREIGN KEY (department_id) REFERENCES department(id),
+    -- FOREIGN KEY (department_id) REFERENCES department(id),
     PRIMARY KEY (id)
 );
 
@@ -23,7 +23,8 @@ CREATE TABLE employee (
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT NULL,
-    FOREIGN KEY (role_id) REFERENCES roles(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    is_manager BOOLEAN NOT NULL,
+    -- FOREIGN KEY (role_id) REFERENCES roles(id),
+    -- FOREIGN KEY (manager_id) REFERENCES employee(id),
     PRIMARY KEY (id)
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -24,36 +24,36 @@ VALUES
             ('Head of HR', 50000, 5),
             ('Secretary', 45000, 5);
 
-INSERT into employee (first_name, last_name, role_id, manager_id)
+INSERT into employee (first_name, last_name, role_id, manager_id, is_manager)
 VALUES
-    ('David', 'Wallace', 1, 1),
-    ('Jan', 'Levinson', 2, 1),
-    ('Michael', 'Scott', 3, 1),
-    ('Angela', 'Martin', 4, 3),
-    ('Oscar', 'Martinez', 5, 4),
-    ('Kevin', 'Malone', 6, 4),
-    ('Dwight', 'Schrute', 7, 3),
-    ('Jim', 'Halpert', 8, 3),
-    ('Andy', 'Bernard', 9, 3),
-    ('Pam', 'Halpert', 10, 3),
-    ('Kelly', 'Kapoor', 11, 1),
-    ('Toby', 'Flenderson', 12, 1),
-    ('Erin', 'Hannon', 13, 3);
+    ('David', 'Wallace', 1, 1, 1),
+    ('Jan', 'Levinson', 2, 1, 1),
+    ('Michael', 'Scott', 3, 1, 1),
+    ('Angela', 'Martin', 4, 3, 0),
+    ('Oscar', 'Martinez', 5, 4, 0),
+    ('Kevin', 'Malone', 6, 4, 0),
+    ('Dwight', 'Schrute', 7, 3, 0),
+    ('Jim', 'Halpert', 8, 3, 0),
+    ('Andy', 'Bernard', 9, 3, 0),
+    ('Pam', 'Halpert', 10, 3, 0),
+    ('Kelly', 'Kapoor', 11, 1, 0),
+    ('Toby', 'Flenderson', 12, 1, 0),
+    ('Erin', 'Hannon', 13, 3, 0);
 
-Select
-employee.first_name,
-employee.last_name,
-roles.title,
-roles.salary,
-department.department_name,
-employee_m.first_name as manager_first_name,
-employee_m.last_name as manager_last_name
-from
-    employee
-    join roles on employee.role_id = roles.id
-    join department on roles.department_id = department.id
-    LEFT join employee as employee_m on employee.manager_id = employee_m.id;
+-- Select
+-- employee.first_name,
+-- employee.last_name,
+-- roles.title,
+-- roles.salary,
+-- department.department_name,
+-- employee_m.first_name as manager_first_name,
+-- employee_m.last_name as manager_last_name
+-- from
+--     employee
+--     join roles on employee.role_id = roles.id
+--     join department on roles.department_id = department.id
+--     LEFT join employee as employee_m on employee.manager_id = employee_m.id;
 
-SELECT * FROM department;
-SELECT * FROM roles;
-SELECT * FROM employee;
+-- SELECT * FROM department;
+-- SELECT * FROM roles;
+-- SELECT * FROM employee;
